1fd14a3e1cfe4729ad246d56dc8a3536d79a762c
taisan <131413638+taisan11@users.noreply.github.com>
2023-12-30 11:15:49 +0900

    ????

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import { Hono } from 'hono'
+import { createClient } from "@supabase/supabase-js";
+type Bindings = {
+    SUPABASE_URL: string
+    SUPABASE_KEY: string
+}
+
+const app = new Hono<{ Bindings: Bindings }>()
+
+app.get('/', async (c) => {
+    const supabase = createClient(c.env.SUPABASE_URL, c.env.SUPABASE_KEY)
+    console.log(c.env.SUPABASE_URL)
+    const { data: test_table, error } = await supabase.from('test_table').select('*')
+  
+    if (error) {
+      console.error(error);
+      return c.json({ status: 500, statusText: 'Error fetching data', data: [] });
+    }
+  
+    if (!test_table || test_table.length === 0 || !test_table[0].naiyou) {
+      return c.json({ status: 404, statusText: 'No data found', data: [] });
+    }
+  
+    return c.json({ status: 200, statusText: 'OK', data: test_table[0].naiyou });
+  });
+export default app
